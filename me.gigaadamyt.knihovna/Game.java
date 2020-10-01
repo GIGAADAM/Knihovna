8c0820d3a0077241386350713b6b37833a75966a
GIGAADAM <47576199+GIGAADAM@users.noreply.github.com>
2020-10-01 19:28:03 +0200

    Create Game.java

---
+++ me.gigaadamyt.knihovna/Game.java
@@ -0,0 +1,11 @@
+public class Game {
+    private Arena arena;
+
+    public Game(Arena arena) {
+        this.arena = arena;
+    }
+
+    public Arena getArena() {
+        return arena;
+    }
+}
